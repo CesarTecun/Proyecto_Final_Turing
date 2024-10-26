c2e61667c884e86fd9fa43c88444a3a93e38edc9
CesarTecun <ctecunl1@miumg.edu.gt>
2024-10-25 21:22:49 -0600

    Initial commit

---
+++ src/main/java/com/mycompany/proyecto_final_turing/Proyecto_Final_Turing.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto_final_turing;
+
+/**
+ *
+ * @author cesar
+ */
+public class Proyecto_Final_Turing {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
